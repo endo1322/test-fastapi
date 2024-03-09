cf448930dbad319dc60ff6eb500dbc009a08669b
bondroid1322 <bondroid3.22@icloud.com>
2024-03-09 23:09:43 +0900

    .

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import HTTPException
+import cv2
+import numpy as np
 
 app = FastAPI()
 
@@ -12,3 +16,15 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/uploadfile")
+async def upload_files(file: UploadFile = File(...)):
+    # アップロードされたファイルの情報を出力
+    print('a')
+    file_info = {
+        "filename": file.filename,
+        "content_type": file.content_type,
+    }
+    print(file_info)
+    return file_info
